5ff87da87845bc0ca973b57e80610c016a6e7c35
loodnn <loudjane@163.com>
2021-07-12 17:50:16 +0800

    init

--- show.js
+++ show.js
@@ -1,12 +1,12 @@
-*{box-sizing: border-box; margin:0;
+const str = `
+.skin *{box-sizing: border-box; margin:0;
     padding:0;}
-*::before, *::after{box-sizing: border-box;}
-
+.skin *::before, .skin *::after{box-sizing: border-box;}
 
 .skin{
+    background:#ffe600;
+    min-height: 50vh;
     position: relative;
-    background: #ffe600;
-    height: 100vh;
 }
 .nose{
     border:10px solid #000;
@@ -206,3 +206,54 @@
     background-color: #ff0000;
     border-radius: 50%;
 }
+`;
+let n = 1; 
+const demo = document.querySelector(".demo");
+const demo2 = document.querySelector(".demo2");
+let id;
+let time = 100;
+const player = {
+  init: () => {
+    demo.innerText = str.substr(0, n);
+    demo2.innerHTML = str.substr(0, n);
+    player.play();
+  },
+  readString: () => {
+    n += 1;
+    if (n > str.length) {
+      window.clearInterval(id);
+      return;
+    }
+    demo.innerText = str.substr(0, n); //文本形式输出
+    demo2.innerHTML = str.substr(0, n); //html标签输出style标签
+    demo.scrollTop = demo.scrollHeight;
+  },
+  play: () => {
+    id = setInterval(player.readString, time);
+  },
+  pause: () => {
+    window.clearInterval(id);
+  },
+  slow: () => {
+    player.pause();
+    time = 300;
+    player.play();
+  },
+  normal: () => {
+    player.pause();
+    time = 100;
+    player.play();
+  },
+  fast: () => {
+    player.pause();
+    time = 0;
+    player.play();
+  },
+};
+
+player.init();
+document.querySelector("#btnPause").onclick = player.pause;
+document.querySelector("#btnPlay").onclick = player.play;
+document.querySelector("#btnSlow").onclick = player.slow;
+document.querySelector("#btnNormal").onclick = player.normal;
+document.querySelector("#btnFast").onclick = player.fast;
